f84d8e8cd8bfbde3192ff3f073dff80bb5f02d26
Dhaval <joshidhaval2002@gmail.com>
2023-09-08 15:09:39 +0530

    . and [] use when, access the object key(property)

--- Object Literals/ObjLiterals.js
+++ Object Literals/ObjLiterals.js
@@ -10,7 +10,7 @@ const movies={
     },
 
     getSpecific:function(details){
-        console.log(movies[details])
+        console.log(movies[details])    //here we have not used '.'to access the object property(key) the reason oft this is in line below the final code
     }
 
 };
@@ -22,3 +22,20 @@ const details='title'
 movies.getSpecific(details)
 movies.getSpecific('genre')
 
+// a={
+//     title:'Hello A',
+//     getSpecific:function(details){
+//         console.log(a[details])
+//     }
+// }
+// b={
+//     title:'Hello B',
+//     getSpecific:function(details){
+//         console.log(b.details)
+//     }
+// }   
+// /*Using Dot Notation b.details:In the getSpecific method of object b, you're trying to access the details property of object b using dot notation (b.details). This won't work as expected because the details property is not defined within object b. Instead, you intended to access a property specified by the details parameter. 
+// To do that correctly, you should use bracket notation as shown in object a. */
+
+// a.getSpecific('title')
+// b.getSpecific('title')
