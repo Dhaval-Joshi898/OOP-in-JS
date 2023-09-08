b16019cd4efecf96a76e22f14e422f39534e9360
Dhaval <joshidhaval2002@gmail.com>
2023-09-08 19:18:59 +0530

    object with this. inside object function

--- this_of _Object/this._object.js
+++ this_of _Object/this._object.js
@@ -1,18 +1,35 @@
 //The below code is wriiten with using object name to acces its properties
-const movies={
+// const movies={
+//     title:'The Avengers',
+//     year:2012,
+//     genre:'Action,Sci-Fi ,Thriller',
+//     cast:'Robert Downey Jr,Chris Evans',
+//     //The below code is one way of writing function inside the Object
+//     getDetails:function(){
+//         console.log(`Title:${movies.title}\nYear:${movies.year}\nGenre:${movies.genre}\nCast:${movies.cast}`)
+//     },
+//     //The another way of writing the function inside object is
+//     giveDetails(){
+//         console.log(`Title:${movies.title}\nYear:${movies.year}\nGenre:${movies.genre}\nCast:${movies.cast}`)
+//     }
+// };
+
+// movies.getDetails()
+// movies.giveDetails()
+
+
+//Now writing the code using 'this.' instead using object name inside (OBJECT function).
+
+const movies1={
     title:'The Avengers',
     year:2012,
     genre:'Action,Sci-Fi ,Thriller',
     cast:'Robert Downey Jr,Chris Evans',
-    //The below code is one way of writing function inside the Object
-    getDetails:function(){
-        console.log(`Title:${movies.title}\nYear:${movies.year}\nGenre:${movies.genre}\nCast:${movies.cast}`)
-    },
     //The another way of writing the function inside object is
     giveDetails(){
-        console.log(`Title:${movies.title}\nYear:${movies.year}\nGenre:${movies.genre}\nCast:${movies.cast}`)
+        console.log(`Title:${this.title}\nYear:${this.year}\nGenre:${this.genre}\nCast:${this.cast}`)
+        //the this refers to the object movies its like this object ka title give,genre give
     }
 };
 
-// movies.getDetails()
-movies.giveDetails()
+movies1.giveDetails()
