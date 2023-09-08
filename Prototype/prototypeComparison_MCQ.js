51192f45a35abdece85cbda1cdc0aa5786cdcd63
Dhaval <joshidhaval2002@gmail.com>
2023-09-08 23:27:51 +0530

    prototype comparison code mcq

---
+++ Prototype/prototypeComparison_MCQ.js
@@ -0,0 +1,8 @@
+function Game(name,plat){
+    this.name=name;
+    this.plat=plat
+    
+}
+const fifa=new Game('fif','PS')
+console.log(fifa.__proto__===Game.prototype)
+console.log(fifa.__proto__.__proto__===Object.prototype)
