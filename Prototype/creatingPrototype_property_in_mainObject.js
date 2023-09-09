89030352b78b45601b001df31e4a1b21b1a023fc
Dhaval <joshidhaval2002@gmail.com>
2023-09-09 19:35:59 +0530

    Created a property using prototype so that the func is accesible to any object without affecting its performance

--- Prototype/creatingPrototype_property_in_mainObject.js
+++ Prototype/creatingPrototype_property_in_mainObject.js
@@ -1,9 +1,24 @@
 const Movie=function(title){
     this.title=title;
-    this.getDetails=function(){
-        console.log(`Title of movie is: ${this.title}`)
-    }
+    // this.getDetails=function(){
+    //     console.log(`Title of movie is: ${this.title}`)
+    // }
 }
+
+//Suppose it there are 100 of objects and each objects has the copy of thr getDetails FUnction,
+//this will affect the performance of the function.
+
+// To solve this function perofrmance issue we can create this function in the prototype of its object
+//So that the main object at top will have this function and THIS function will be accesible to all the child object or prototype
+
+//To create it: here the property (key is created ) getDetails Property
+
+Object.prototype.getDetails = function(){
+    console.log(`Title of movie is: ${this.title}`)
+}
+//Now check the developer tool chrome to see this getDetails method is present isnide the prototype
+
+
 let movieOne=new Movie('The Avengers Infinity War')   //Creating object
 console.log(movieOne)
 movieOne.getDetails()
