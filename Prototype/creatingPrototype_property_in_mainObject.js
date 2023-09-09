c7286d89f11a550c7dc662bc0dcb4db7bbf46c35
Dhaval <joshidhaval2002@gmail.com>
2023-09-09 19:23:29 +0530

    Created a Function  property inside the object

--- Prototype/creatingPrototype_property_in_mainObject.js
+++ Prototype/creatingPrototype_property_in_mainObject.js
@@ -1,8 +1,12 @@
 const Movie=function(title){
     this.title=title;
+    this.getDetails=function(){
+        console.log(`Title of movie is: ${this.title}`)
+    }
 }
 let movieOne=new Movie('The Avengers Infinity War')   //Creating object
 console.log(movieOne)
+movieOne.getDetails()
 
 let movieTwo=new Movie('The Avatar')   //Creating object
 console.log(movieTwo)
