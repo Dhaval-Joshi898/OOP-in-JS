91227fd0930e71b8ab2319eaf36f73bcfdacd3e3
Dhaval <joshidhaval2002@gmail.com>
2023-09-09 19:21:35 +0530

    toget prototypeobject Object.getprototype(nameObj)

--- Prototype/PrototypeChain.js
+++ Prototype/PrototypeChain.js
@@ -4,3 +4,6 @@ const car={
 }
 console.log(car.__proto__)  //this will give the main object (top of the heirarchy)
 //All the functions or the properties which are htere in the main object can be accesible BY THIS OBJECT LITERAL CAR
+
+// TO check Prototype of any object we can use:
+Object.getPrototypeOf(car) //run this in console  {you will get the top object in hierrarchy}
