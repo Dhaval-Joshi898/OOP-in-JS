2f2a737b693fdc322a9ed548bb9359b91f0b734f
Dhaval <joshidhaval2002@gmail.com>
2023-09-09 19:09:29 +0530

    Object literal  prototype

---
+++ Prototype/PrototypeChain.js
@@ -0,0 +1,6 @@
+//Below is the object literal
+const car={
+    carName:'BMW'
+}
+console.log(car.__proto__)  //this will give the main object (top of the heirarchy)
+//All the functions or the properties which are htere in the main object can be accesible BY THIS OBJECT LITERAL CAR
