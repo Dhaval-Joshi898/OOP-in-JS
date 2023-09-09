307ef702d19434edd6ae73404e4b973c083ac2d6
Dhaval <joshidhaval2002@gmail.com>
2023-09-09 19:22:01 +0530

    Created a constructor and two objects

---
+++ Prototype/creatingPrototype_property_in_mainObject.js
@@ -0,0 +1,8 @@
+const Movie=function(title){
+    this.title=title;
+}
+let movieOne=new Movie('The Avengers Infinity War')   //Creating object
+console.log(movieOne)
+
+let movieTwo=new Movie('The Avatar')   //Creating object
+console.log(movieTwo)
